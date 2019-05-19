c7f3747cf2330197158a6ad57a68f76b20810a00
xuanxuanxuan68 <1598966277@qq.cpm>
2019-05-19 20:24:05 +0800

    This is first commit
    create by xuanxuanxuan68
    version 0.1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("c++");
+        //System.out.println
+    }
+}
